733e1f6147206a219011feb4687ab1040f36201c
Staffan Ösp <staffan.osp@gmail.com>
2023-03-07 10:23:43 +0100

    ConsellorGallery placeholder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import DataDemo from "./data/DataDemo";
+import CounsellorGallery from "./components/CounsellorGallery";
 
 function App() {
   return (
     <>
       <h1>Welcome to Handledning.se</h1>
-      <DataDemo />
+      <CounsellorGallery />
+      {/* <DataDemo /> */}
     </>
   );
 }
---
+++ src/components/CounsellorGallery.jsx
@@ -0,0 +1,9 @@
+const CounsellorGallery = () => {
+  return (
+    <>
+      <h2>GALLERY!</h2>
+    </>
+  );
+};
+
+export default CounsellorGallery;
