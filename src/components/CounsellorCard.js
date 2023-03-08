6b32ee51d4c2f4f2ddcb47909ff01a913735ab7a
annaBohlin <anna@bohlinoffice.se>
2023-03-08 11:50:45 +0100

    removed inline styles from councellor card

--- src/components/CounsellorCard.js
+++ src/components/CounsellorCard.js
@@ -12,19 +12,21 @@ const CounsellorCard = ({
 }) => {
   return (
     <>
-      <img src={avatarUrl} alt="" />
-      <h3>{firstName}</h3>
-      <p>{location}</p>
-      <h4>{title}</h4>
-      <p>{ratePerHour}</p>
-      {/*area tags container, remove inline style :) */}
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "4px" }}>
-        {areas.map((area) => (
-          <Tag>{area}</Tag>
-        ))}
-      </div>
-      <button onClick={() => showProfile(id)}>Visa Profil</button>
-    </>
+			<div>
+				<img src={avatarUrl} alt="" />
+				<h3>{firstName}</h3>
+				<p>{location}</p>
+				<h4>{title}</h4>
+				<p>{ratePerHour}</p>
+				{/*area tags container, remove inline style :) */}
+				<div style={{ display: "flex", flexWrap: "wrap", gap: "4px" }}>
+					{areas.map((area) => (
+						<Tag>{area}</Tag>
+					))}
+				</div>
+				<button onClick={() => showProfile(id)}>Visa Profil</button>
+			</div>
+		</>
   );
 };
 
