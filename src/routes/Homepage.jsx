04052044a18b095df7eafb39370bee5b7580402d
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-03-20 21:08:53 +0100

    Homepage hero image

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -3,4 +3,18 @@ const Homepage = () => {
   return <h2>"Homepage"</h2>;
 };
 
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="hero-image">
+        <img src="path/to/hero/image.jpg" alt="Hero Image" />
+        <div className="text-overlay">
+          <h1>Hitta handledare över hela Sverige</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
 export default Homepage;
