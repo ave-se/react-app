ec7f6d651a43e5069147d92f5cf43871d320c436
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-03-20 21:28:42 +0100

    added unsplash image to homepage jsx

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,17 +1,27 @@
 import "../styles/Homepage.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 function HomePage() {
+  const unsplashUrl = 'https://source.unsplash.com/random/1512x608';
+
   return (
-    <div className="container">
-      <div className="hero-image">
-        <img src="path/to/hero/image.jpg" alt="Hero Image" />
-        <div className="text-overlay">
-          <h1>Hitta handledare över hela Sverige</h1>
-        </div>
+    <div className="container position-relative">
+      <img
+        src={unsplashUrl}
+        alt="Hero"
+        className="w-100"
+      />
+      <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-between">
+        <button className="btn btn-link text-white">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button className="btn btn-link text-white">
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
       </div>
     </div>
   );
 }
 
-
-export default Homepage;
+export default HomePage;
