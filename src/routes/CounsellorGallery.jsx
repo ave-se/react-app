39c08e16c83d53a0c23c27b814dc02b771da94d9
Staffan Ösp <staffan.osp@gmail.com>
2023-03-20 19:30:50 +0100

    Hero section in gallery

--- src/routes/CounsellorGallery.jsx
+++ src/routes/CounsellorGallery.jsx
@@ -12,6 +12,7 @@ import {
 import "../styles/CounsellorGallery.css";
 
 import iconSearch from "../assets/icon_search.svg";
+import galleryIllustration from "../assets/gallery-illustration.svg";
 
 export async function loader({ request }) {
   const makeSearchArray = (param) => {
@@ -89,82 +90,108 @@ const CounsellorGallery = () => {
 
   return (
     <div className="counsellor-gallery-wrapper">
+      <div className="hero">
+        <div className="col-2">
+          <div className="row-2">
+            <h2>Hitta handledaren för dig och din grupp hos oss!</h2>
+            <p>
+              Bläddra vidare för att se handledare och vårt nya
+              filteringssystem.
+            </p>
+          </div>
+          <img src={galleryIllustration} alt="Gallery Illustration"></img>
+
+          <div></div>
+          <div className="row-2">
+            <h2>Har du frågor eller behöver hjälp att hitta rätt?</h2>
+            <div>
+              <a className="button">Till formulär</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h2>Hitta handledare över hela Sverige!</h2>
       <Form id="search-form" role="search" onSubmit={(e) => {}}>
-        {searchIsActive && (
-          <Link
-            className="clear-filters"
-            to=".."
-            relative="path"
-            onClick={(e) => {
-              e.preventDefault();
-              clearFilters();
-            }}
-          >
-            Rensa alla filter
-          </Link>
-        )}
-
-        <div className="input_with_label">
-          <label htmlFor="filterFreeText">
-            <img src={iconSearch} alt="search icon"></img>
-          </label>
-          <input
-            id="filterFreeText"
-            name="filterFreeText"
-            placeholder="Sök..."
-            value={filterFreeText}
+        <div className="inputs-wrapper">
+          <div className="input_with_label">
+            <label htmlFor="filterFreeText">
+              <img src={iconSearch} alt="search icon"></img>
+            </label>
+            <input
+              id="filterFreeText"
+              name="filterFreeText"
+              placeholder="Sök..."
+              value={filterFreeText}
+              onChange={(e) => {
+                setFilterFreeText(e.target.value);
+                submitOnChange(e.target.form);
+              }}
+            />
+          </div>
+          <select
+            name="filterLocations"
+            value={filterLocations}
             onChange={(e) => {
-              setFilterFreeText(e.target.value);
+              setFilterLocations(e.target.value);
               submitOnChange(e.target.form);
             }}
-          />
+          >
+            <option value="">Region</option>
+            {locationsOptions.map((location, i) => (
+              <option key={i} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
+          <select
+            name="filterAreas"
+            value={filterAreas}
+            onChange={(e) => {
+              setFilterAreas(e.target.value);
+              submitOnChange(e.target.form);
+            }}
+          >
+            <option value="">Specialområde</option>
+            {areasOptions.map((area, i) => (
+              <option key={i} value={area}>
+                {area}
+              </option>
+            ))}
+          </select>
+          <select
+            name="filterRemoteOptions"
+            value={filterRemoteOptions}
+            onChange={(e) => {
+              setFilterRemoteOptions(e.target.value);
+              submitOnChange(e.target.form);
+            }}
+          >
+            <option value="">I lokal/digitalt</option>
+            {remoteOptionsOptions.map((remoteOption, i) => (
+              <option key={i} value={remoteOption}>
+                {remoteOption}
+              </option>
+            ))}
+          </select>
         </div>
-        <select
-          name="filterLocations"
-          value={filterLocations}
-          onChange={(e) => {
-            setFilterLocations(e.target.value);
-            submitOnChange(e.target.form);
-          }}
-        >
-          <option value="">Region</option>
-          {locationsOptions.map((location, i) => (
-            <option key={i} value={location}>
-              {location}
-            </option>
-          ))}
-        </select>
-        <select
-          name="filterAreas"
-          value={filterAreas}
-          onChange={(e) => {
-            setFilterAreas(e.target.value);
-            submitOnChange(e.target.form);
-          }}
-        >
-          <option value="">Specialområde</option>
-          {areasOptions.map((area, i) => (
-            <option key={i} value={area}>
-              {area}
-            </option>
-          ))}
-        </select>
-        <select
-          name="filterRemoteOptions"
-          value={filterRemoteOptions}
-          onChange={(e) => {
-            setFilterRemoteOptions(e.target.value);
-            submitOnChange(e.target.form);
-          }}
-        >
-          <option value="">I lokal/digitalt</option>
-          {remoteOptionsOptions.map((remoteOption, i) => (
-            <option key={i} value={remoteOption}>
-              {remoteOption}
-            </option>
-          ))}
-        </select>
+
+        <div className="clear-filters">
+          {searchIsActive ? (
+            <Link
+              to=".."
+              relative="path"
+              onClick={(e) => {
+                e.preventDefault();
+                clearFilters();
+              }}
+            >
+              Rensa alla filter
+            </Link>
+          ) : (
+            <>&nbsp;</>
+          )}
+        </div>
       </Form>
 
       <div className="counsellor-gallery-cards-wrapper">
