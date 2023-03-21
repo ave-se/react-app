5cdb6b73a011a1b949c5c9e7324e0ea9f4b67446
annaBohlin <anna@bohlinoffice.se>
2023-03-21 11:34:29 +0100

    added routes

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,4 +1,5 @@
 import "../styles/Contact.css"
+import { Link } from "react-router-dom";
 
 import contactIllustrationLeft from "../assets/contact-illustration-left.svg";
 import contactIllustrationMiddle from "../assets/contact-illustration-middle.svg";
@@ -9,40 +10,46 @@ const Contact = () => {
     <div>
       <div className="contact-cards-wrapper">
         <div className="contact-card item-a">
-          <img 
-          src={contactIllustrationLeft} 
-          alt="Illustration of a person searching for a counsellor with a magnifying glass" 
-          />
-          <h2>Vilka är vi?</h2>
+          <Link to="/about">
+            <img 
+              src={contactIllustrationLeft} 
+              alt="Illustration of a person searching for a counsellor with a magnifying glass" 
+            />
+            <h2>Vilka är vi?</h2>
+          </Link>
         </div>
         <div className="contact-card item-b">
-          <img 
-          src={contactIllustrationMiddle} 
-          alt="Illustration of a person sitting on the floor with a laptop on their 
-          lap with chat bubbles next to their head" 
-          />
-          <h2>Vad är <br/>handledning.se?</h2>
+          <Link to="/about">
+            <img 
+              src={contactIllustrationMiddle} 
+              alt="Illustration of a person sitting on the floor with a laptop on their 
+              lap with chat bubbles next to their head" 
+            />
+            <h2>Vad är <br/>handledning.se?</h2>
+          </Link>
         </div>
         <div className="contact-card item-c">
-          <img 
-          src={contactIllustrationRight} 
-          alt="Illustration of a counsellor and a patient takling in an office" 
-          />
-          <h2>Vi kan hjälpa dig med <br/>att hitta handledare</h2>
+          <Link to="/gallery">
+            <img 
+              src={contactIllustrationRight} 
+              alt="Illustration of a counsellor and a patient takling in an office" 
+            />
+            <h2>Vi kan hjälpa dig med <br/>att hitta handledare</h2>
+          </Link>
         </div>
       </div>
       <div className="contact-links-wrapper">
-        <div className="contact-link">
+        <div className="contact-bubble">
           <p className="bold">Är du handledare och vill vara med på hemsidan?</p>
-          <a href="">Till registrera</a>
+          <Link to={""}>Till registrera</Link>
         </div>
-        <div className="contact-link">
+        <div className="contact-bubble">
           <p className="bold">Emaila din handledare!</p>
-          <a href="">Till formuläret</a>
+          <Link to={""}>Till formuläret</Link>
         </div>
-        <div className="contact-link">
+        <div className="contact-bubble">
           <p className="bold">Ring oss om frågor!</p>
-          <a href="">Mer om kontakt</a>
+          <Link to={""}>Mer om kontakt</Link>
         </div>
       </div>
     </div>
