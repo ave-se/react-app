e0169ea55aab3df0dc2166475f019daa5cc9c354
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-03-21 12:59:57 +0100

    styled hero image some more

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,14 +1,24 @@
 import "../styles/Homepage.css";
-import navIcon from "../assets/nav-icon.svg";
-const Homepage = () => {
+import navIconLeft from "../assets/nav-icon-left.svg";
+import navIconRight from "../assets/nav-icon-right.svg";
+
+function Homepage() {
   return (
-    <div className="home-page" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')` }}>
-      <h1>Hitta handledare över hela Sverige</h1>
-       <img src={navIcon} alt="Left Icon" className="left-button" />
-     <img src={navIcon} alt="Right Icon" className="right-button" />
+    <div className="homepage">
+      <div className="hero">
+        <img src="https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Hero Image" />
+        <div className="hero-nav">
+          <img src={navIconLeft} alt="Left Nav" className="left-nav" />
+          <img src={navIconRight} alt="Right Nav" className="right-nav" />
+        </div>
+        <div className="hero-text">
+          <h1>Hitta handledare över hela Sverige</h1>
+        </div>
+      </div>
     </div>
   );
 };
+
 
 export default Homepage;
 
