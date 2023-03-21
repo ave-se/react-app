7a8d0eeb3950b8d7d7a255f19b7f79fac4cfe3ea
Staffan Ösp <staffan.osp@gmail.com>
2023-03-21 12:53:04 +0100

    Select should accept className being passed as prop

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,11 +1,11 @@
-const Select = ({ children, name, value, onChange }) => {
+const Select = ({ children, className, name, value, onChange }) => {
+  className = className ?? "";
+  if (value !== "") {
+    className += " active";
+  }
+
   return (
-    <select
-      className={value !== "" ? "active" : undefined}
-      name={name}
-      value={value}
-      onChange={onChange}
-    >
+    <select className={className} name={name} value={value} onChange={onChange}>
       {children}
     </select>
   );
