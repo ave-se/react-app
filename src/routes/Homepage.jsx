2e73fffe4527abe3916407802365fc619f78e3bd
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-03-21 20:17:05 +0100

    test

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -6,7 +6,11 @@ function Homepage() {
   return (
     <div className="homepage">
       <div className="hero">
+<<<<<<< HEAD
         <img src="https://source.unsplash.com/random/503x500?professional?portrait" alt="Hero Image" />
+=======
+        <img src="https://images.unsplash.com/photo-1615461065624-21b562ee5566?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80" alt="Hero Image" />
+>>>>>>> 32091c7 (test)
         <div className="hero-nav">
           <img src={navIconLeft} alt="Left Nav" className="left-nav" />
           <img src={navIconRight} alt="Right Nav" className="right-nav" />
@@ -15,10 +19,58 @@ function Homepage() {
           <h1>Hitta handledare över hela Sverige</h1>
         </div>
       </div>
+<<<<<<< HEAD
+=======
+
+
+
+      <div className="lasMer">
+        <h2>Läs mer om vad handledning är och varför det ger utveckling. I vår handledarförteckning hittar ni enkelt er nästa handledare.</h2>
+      </div>
+
+     <div className="stats-container">
+  <div className="fiftyThree">
+    <h2>53</h2>
+    <p>Registrerade Handledare att välja mellan</p>
+  </div>
+
+  <div className="oneHundred">
+    <h2>100%</h2>
+    <p>Av Svergies olika län</p>
+  </div>
+</div>
+
+
+
+
+      
+      <div className="secondImage">
+         <img src="https://images.unsplash.com/photo-1622227922682-56c92e523e58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Second Image" />
+       <div className="second-image-text" ><p>“Handledning är en självklar kvalitetssäkring för vårt arbete och ger oss samtidigt inspiration och möjligheter till personlig utveckling.” Anna, Enhetschef</p>
+      </div>
+      </div>
+
+
+
+
+      <div className="section">
+        <h2>Section 5</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div className="section">
+        <h2>Section 6</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div className="section">
+        <h2>Section 7</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+>>>>>>> 32091c7 (test)
     </div>
   );
 };
 
 
 export default Homepage;
+
 
